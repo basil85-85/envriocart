1548094967a0ee02162c8684cd68c1e90bb670a0
basil85-85 <84588091+basil85-85@users.noreply.github.com>
2025-03-08 21:14:19 +0700

    authgoogle

--- config/googleauth.js
+++ config/googleauth.js
@@ -10,7 +10,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:9000/auth/google/callback",
+    callbackURL: "https://envriocart.shop/auth/google/callback",
     // passReqToCallback: true
   },
   // function(request, accessToken, refreshToken, profile, done) {
